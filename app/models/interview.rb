8d4ca7ea59d9108b28079070910ebc545788e72c
Dmitriy Dudkin <dudkin.dmitriy@gmail.com>
2016-04-01 14:09:09 +0300

    Styled interviews pages

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,5 +1,14 @@
 class Interview < ActiveRecord::Base
   has_many :answers
+  has_many :questions, through: :answers
 
   validates :name, :appointed_at, presence: true
+
+  def questions_count_by_difficulty
+    counts = questions.group(:difficulty).count
+
+    Question.difficulties.map do |name, value|
+      [name, counts[value] || 0]
+    end
+  end
 end
