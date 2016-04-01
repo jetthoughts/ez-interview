4340eb077ca89de62dfba0f9bf15140a36bfea1f
Dmitriy Dudkin <dudkin.dmitriy@gmail.com>
2016-04-01 15:38:45 +0300

    Styled the total score

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,18 @@ module ApplicationHelper
     )
     markdown.render(text).html_safe
   end
+
+  def mark_label_class(mark)
+    if mark < 20
+      'label-danger'
+    elsif 20 < mark && mark < 40
+      'label-warning'
+    elsif 40 < mark && mark < 60
+      'label-info'
+    elsif 60 < mark && mark < 80
+      'label-primary'
+    elsif 80 < mark && mark < 100
+      'label-success'
+    end
+  end
 end
